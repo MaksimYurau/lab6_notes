12adb7822a36a33adc3fbce89cc388524b48a0c2
MaksimYurau <2004softwaredeveloper@gmail.com>
2024-04-15 15:35:37 +0300

    Updated project. Updated SaveNoteScreen.kt. Added vcs.xml.

--- app/src/main/java/com/maksimyurau/android/lab6_notes/ui/components/screens/SaveNoteScreen.kt
+++ app/src/main/java/com/maksimyurau/android/lab6_notes/ui/components/screens/SaveNoteScreen.kt
@@ -1,4 +1,4 @@
-package com.maksimyurau.android.lab6_notes.ui
+package com.maksimyurau.android.lab6_notes.ui.components.screens
 
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
