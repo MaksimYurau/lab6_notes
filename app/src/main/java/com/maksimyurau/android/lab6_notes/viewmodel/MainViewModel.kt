bee13ff9ef99fdad91f9d8f3cb3181fe0d839990
MaksimYurau <2004softwaredeveloper@gmail.com>
2024-04-15 16:02:51 +0300

    Updated project. Updated MainViewModel.kt.

--- app/src/main/java/com/maksimyurau/android/lab6_notes/viewmodel/MainViewModel.kt
+++ app/src/main/java/com/maksimyurau/android/lab6_notes/viewmodel/MainViewModel.kt
@@ -1,7 +1,9 @@
 package com.maksimyurau.android.lab6_notes.viewmodel
 
+import androidx.lifecycle.LiveData
 import androidx.lifecycle.ViewModel
 import com.maksimyurau.android.lab6_notes.data.repository.Repository
+import com.maksimyurau.android.lab6_notes.model.NoteModel
 
 /**
  * Модель просмотра, используемая для хранения глобального состояния приложения.
@@ -9,5 +11,7 @@ import com.maksimyurau.android.lab6_notes.data.repository.Repository
  * Эта модель просмотра используется для всех экранов.
  */
 class MainViewModel(private val repository: Repository) : ViewModel() {
-
+    val notesNotInTrash: LiveData<List<NoteModel>> by lazy {
+        repository.getAllNotesNotInTrash()
+    }
 }
