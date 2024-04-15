9e6370005515314b5c4319037a03d8ee50bbc08c
MaksimYurau <2004softwaredeveloper@gmail.com>
2024-04-15 15:38:14 +0300

    Updated project. Added NotesScreen.kt. Added fun NotesScreen().

---
+++ app/src/main/java/com/maksimyurau/android/lab6_notes/ui/components/screens/NotesScreen.kt
@@ -0,0 +1,11 @@
+package com.maksimyurau.android.lab6_notes.ui.components.screens
+
+import androidx.compose.runtime.Composable
+import com.maksimyurau.android.lab6_notes.viewmodel.MainViewModel
+
+@Composable
+fun NotesScreen(
+    viewMode: MainViewModel
+) {
+
+}
